da59163c1cd806eb1b5252574f7b8b18f052f370
Renan Gimenez <renangiimenez@gmail.com>
2024-10-24 20:43:31 -0300

    chore: setup routes

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { AuthProvider, useAuth } from "@/contexts/AuthContext";
+
+import { Home, Login } from "@/screens";
+
+const AuthenticatedApp = () => {
+  const { isAuthenticated } = useAuth();
+
+  return isAuthenticated ? <Home /> : <Login />;
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider>
+      <AuthenticatedApp />
+    </AuthProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
