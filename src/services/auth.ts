ccd5032fb53cfab4112bfaf5e5854e05b8019b91
Renan Gimenez <renangiimenez@gmail.com>
2024-10-26 23:56:47 -0300

    feat: implement signup logic

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -5,16 +5,52 @@ interface LoginProps {
   password: string;
 }
 
-interface RequestProps {
+interface LoginRequestProps {
+  token: string;
+  user: any;
+}
+
+interface SignupProps {
+  username: string;
+  email: string;
+  password: string;
+}
+
+interface SignupRequestProps {
   token: string;
   user: any;
 }
 
 export const loginRequest = async ({ username, password }: LoginProps) => {
   try {
-    const response = await api.post<RequestProps>("/login", {
+    const response = await api.post<LoginRequestProps>("/login", {
       username,
       password,
+    });
+
+    return {
+      token: response.data.token,
+      user: response.data.user,
+    };
+  } catch (error: any) {
+    if (error.response && error.response.data) {
+      throw Error(error.response.data.message);
+    } else {
+      console.error(error.message);
+    }
+  }
+};
+
+export const signupRequest = async ({
+  username,
+  email,
+  password,
+}: SignupProps) => {
+  try {
+    const response = await api.post<SignupRequestProps>("/create-user", {
+      username,
+      password,
+      email,
     });
 
     return {
