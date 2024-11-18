13f90b36a8115e7e1fc06bce82e7d9a73ee7cf4e
Renan Gimenez <renangiimenez@gmail.com>
2024-11-17 22:30:43 -0300

    feat: enable new architecture

---
+++ app.config.js
@@ -0,0 +1,41 @@
+console.log({ NODE_ENV: process.env.NODE_ENV });
+
+const getName = () => {
+  switch (process.env.NODE_ENV) {
+    case "production":
+      return "auth-jwt";
+    default:
+      return "auth-jwt (Dev)";
+  }
+};
+
+export default ({ config }) => ({
+  ...config,
+  expo: {
+    name: getName(),
+    slug: "auth-jwt",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    plugins: ["expo-secure-store"],
+    newArchEnabled: true,
+  },
+});
