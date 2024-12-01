5040900b9218da213654d2ce35143a05ea32e4da
Renan Gimenez <renangiimenez@gmail.com>
2024-11-30 23:02:21 -0300

    feat: improve input component

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,17 +1,18 @@
 import { colors } from "@/styles/colors";
-import { Platform, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
+    minHeight: 46,
     flexDirection: "row",
     alignItems: "center",
     borderRadius: 4,
     backgroundColor: colors.gray[800],
-    paddingVertical: Platform.OS === "ios" ? 12 : 4,
     paddingHorizontal: 16,
     gap: 12,
   },
   textInput: {
+    height: "100%",
     flex: 1,
     color: colors.gray[100],
     fontSize: 14,
